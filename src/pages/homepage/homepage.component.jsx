b49710f8069a37c2927b43cf3ca00dcb183fbf34
Seder <nicu.seder@dbschenker.com>
2020-09-24 10:48:50 +0300

    hompepage splited in smaller components and styled the menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+//Import styles
+import './homepage.styles.scss';
+
+//Import components
+import Directory from "../../components/directory/directory.component";
+
+const HomePage = ()=>{
+    return(
+    <div className={'homepage'}>
+        <Directory/>
+    </div>
+    )
+};
+
+export default HomePage;
