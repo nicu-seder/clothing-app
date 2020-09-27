2ed9509d7ebd9e944c2da5af4229bd47d8c7648a
Seder <nicu.seder@dbschenker.com>
2020-09-27 17:44:06 +0300

    cartItem component added. cartDropdown is updating with the added cartItems

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,18 +1,32 @@
 import React from "react";
-
+import {connect} from 'react-redux';
 //Import styles
 import './cart-dropdown.styles.scss';
 
 //Import components
 import {CustomButton} from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = ()=>{
-    return(
+
+const CartDropdown = ({cartItems}) => {
+    return (
         <div className={'cart-dropdown'}>
-            <div className={'cart-items'}/>
+            <div className={'cart-items'}>
+                {
+                    cartItems.map(cartItem => {
+                        return <CartItem key={cartItem.id} item={cartItem}/>
+                    })
+                }
+            </div>
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     )
 };
 
-export default CartDropdown;
+const mapStateToProps = state => {
+    return {
+        cartItems: state.cart.cartItems
+    }
+};
+
+export default connect(mapStateToProps, null)(CartDropdown);
